eb954284672c5fd84d9edccfa4ccf413efc8e303
yipchal <104343492+yipchal@users.noreply.github.com>
2024-06-27 11:20:02 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  pages: {
+    index: {
+      entry: 'examples/main.js',
+      template: 'public/index.html',
+       flliename: 'index.html'
+    }
+  },
+  configureWebpack: {
+    plugins: []
+  }
+}
